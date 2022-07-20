f7c08fc875cd7f788ea015826caf3dd6d59bd5c4
Jeff Hokit <jhokit@rocketcom.com>
2022-07-20 14:19:18 -0700

    fix: try view tint color

--- Sources/AstroSwiftFoundation/Views/Status.swift
+++ Sources/AstroSwiftFoundation/Views/Status.swift
@@ -50,7 +50,9 @@ public class StatusView: UIImageView {
         // The Status images are SFSymbols with custom colors, and should automatically draw in color,
         // but due to a bug in the internal SVG code that the SFSymbols app write our the custom color,
         // they render in black. So, as a workaround, reapply the status color through withTintColor
-        self.image = UIImage.imageForAstroStatus(status).withTintColor(UIColor(status.color), renderingMode: .alwaysTemplate))
+        self.image = UIImage.imageForAstroStatus(status).withRenderingMode(.alwaysTemplate)
+        self.tintColor = UIColor(status.color)
+
     }
 }
 #endif
@@ -68,7 +70,8 @@ public class StatusView: WKInterfaceImage {
         // The Status images are SFSymbols with custom colors, and should automatically draw in color,
         // but due to a bug in the internal SVG code that the SFSymbols app write our the custom color,
         // they render in black. So, as a workaround, reapply the status color through withTintColor
-        self.setImage(UIImage.imageForAstroStatus(status).withTintColor(UIColor(status.color), renderingMode: .alwaysTemplate))
+        self.image = UIImage.imageForAstroStatus(status).withRenderingMode(.alwaysTemplate)
+        self.tintColor = UIColor(status.color)
     }
 }
 #endif
