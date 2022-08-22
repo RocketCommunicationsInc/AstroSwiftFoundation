6933b4c920603bca03bf1c7ad61e5f6ed2d905d7
Jeff Hokit <jhokit@rocketcom.com>
2022-08-22 15:30:15 -0700

    build: test alternate color value

--- Sources/AstroSwiftFoundation/Views/Tag.swift
+++ Sources/AstroSwiftFoundation/Views/Tag.swift
@@ -51,7 +51,7 @@ public struct Tag: View {
     public var body: some View {
         
 #if canImport(UIKit)
-        let textColor = Color(UIColor.label)
+        let textColor = Color(.red)
 #elseif canImport(AppKit)
         let textColor = Color(.labelColor)
 #endif
